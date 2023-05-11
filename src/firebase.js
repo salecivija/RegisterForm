0d11a4319678c965020c04958fd1a0173d158ab7
Aleksandar <salecivija@gmail.com>
2023-05-11 15:02:42 +0200

    v1.2.1
    firebase config edited for public

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getDatabase } from "firebase/database"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAFP2UP4L_sAGyL1W3ENOGXLjnDXBJRsbI",
-  authDomain: "registerform-1bdd4.firebaseapp.com",
-  projectId: "registerform-1bdd4",
-  storageBucket: "registerform-1bdd4.appspot.com",
-  messagingSenderId: "662789269347",
-  appId: "1:662789269347:web:5b27b45fba6f4469059556",
-  measurementId: "G-67Y41T36RC"
+  apiKey: "******",
+  authDomain: "******",
+  projectId: "*******",
+  storageBucket: "*******",
+  messagingSenderId: "******",
+  appId: "*******",
+  measurementId: "*********"
 };
 
 // Initialize Firebase
